6bfbfa76242d15650188ae77c764108d439067e1
pyqlsa <26353308+pyqlsa@users.noreply.github.com>
2024-01-21 11:04:34 -0800

    dhcp for tank

--- hosts/tank/default.nix
+++ hosts/tank/default.nix
@@ -26,22 +26,24 @@
     domain = "nochtlabs.net";
     hostId = "d63e4a0a";
     networkmanager.enable = true;
-    useDHCP = false;
-    interfaces = {
-      enp10s0f0np0 = {
-        useDHCP = false;
-        ipv4.addresses = [
-          {
-            address = "10.10.1.50";
-            prefixLength = 24;
-          }
-        ];
-      };
-    };
-    defaultGateway = "10.10.1.1";
-    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    useDHCP = lib.mkDefault true;
+    #useDHCP = false;
+    #interfaces = {
+    #  enp10s0f0np0 = {
+    #    useDHCP = false;
+    #    ipv4.addresses = [
+    #      {
+    #        address = "10.10.1.50";
+    #        prefixLength = 24;
+    #      }
+    #    ];
+    #  };
+    #};
+    #defaultGateway = "10.10.1.1";
+    #nameservers = [ "1.1.1.1" "1.0.0.1" ];
   };
 
+  # networking.interfaces.enp10s0f0np0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp10s0f1np1.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp11s0u9u3c2.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
