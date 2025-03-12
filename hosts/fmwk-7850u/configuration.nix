c49166217feb809464f407725feaa3c5205f6061
pyqlsa <26353308+pyqlsa@users.noreply.github.com>
2025-03-11 21:37:26 -0700

    proton

--- hosts/fmwk-7850u/configuration.nix
+++ hosts/fmwk-7850u/configuration.nix
@@ -97,7 +97,7 @@
       commonOpts = {
         updateResolvConf = true;
         server = {
-          address = "146.70.174.146";
+          address = "89.45.4.2";
           ports = [ 51820 5060 80 4569 1194 ];
         };
         openvpnCreds = config.sops.secrets."vpn/protonvpn/creds".path;
